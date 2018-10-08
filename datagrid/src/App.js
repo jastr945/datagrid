17f335b90c948242fc762200caa98492568f380b
jastr945 <polina.jastr@gmail.com>
2018-10-07 21:07:16 -0700

    React datagrid skeleton

---
+++ datagrid/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './Example.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Example />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ datagrid/src/Example.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReactDataGrid from 'react-data-grid';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class Example extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.createRows();
+    this._columns = [
+      { key: 'id', name: 'ID' },
+      { key: 'title', name: 'Title' },
+      { key: 'count', name: 'Count' } ];
+
+    this.state = null;
+  }
+
+  createRows = () => {
+    let rows = [];
+    for (let i = 1; i < 1000; i++) {
+      rows.push({
+        id: i,
+        title: 'Title ' + i,
+        count: i * 1000
+      });
+    }
+
+    this._rows = rows;
+  };
+
+  rowGetter = (i) => {
+    return this._rows[i];
+  };
+
+  render() {
+    return  (
+      <ReactDataGrid
+        columns={this._columns}
+        rowGetter={this.rowGetter}
+        rowsCount={this._rows.length}
+        minHeight={500} />);
+  }
+}
+
+export default Example;
